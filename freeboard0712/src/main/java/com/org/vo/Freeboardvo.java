238ee4621ee1fe13fa74e3a605e42cfa251fc1d7
hanYijin <gksdlwls123@naver.com>
2021-07-13 14:00:55 +0900

    spring

---
+++ freeboard0712/src/main/java/com/org/vo/Freeboardvo.java
@@ -0,0 +1,12 @@
+package com.org.vo;
+
+public class Freeboardvo {
+	private int idx;
+	private String title;
+	private String pass;
+	private String content;
+	private String wdate;
+	private String name;
+	private String email;
+	private int see;
+}
